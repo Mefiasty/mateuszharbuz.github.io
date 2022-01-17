d1603263a87ef0ab7a4ef6a640874d4a1d28ff4d
Mefiasty <97467781+Mefiasty@users.noreply.github.com>
2022-01-17 21:20:38 +0100

    Add files via upload

--- php.php
+++ php.php
@@ -6,20 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <!-- /////////////////////////////////////góra////////////////////////////// -->
+<div class="goragora">
 <div class="gora">
-    <h1 class="skryptphp">Skrypt PHP Bez Bazy Danych<h1>
+    <h1 class="skryptphp">Skrypt PHP<h1>
+</div>
 </div>
 <!-- ///////////////////////////////środek///////////////////////////// -->
 <div class="bloczek">
     <div class="opis">
     <h2>Przelicznik Walutowy</h2>
     <form ACTION="" method="post">
-Wpisz ilość w PLN:<input TYPE="text" name="zlote"> --- <INPUT TYPE="submit" VALUE="Oblicz"><br><br>
+Wpisz ilość w PLN: <input TYPE="number" name="zlote" style="margin-left:1em;" step="0.01"> --- <INPUT TYPE="submit" VALUE="Oblicz"><br><br>
 </form>
-<center>
+
 <p>
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
@@ -35,25 +43,25 @@ $koronadu = 0.51;
 $dolark = 2.80;
 $rubel = 0.11;
 $koronasz = 0.41;
-$wynikf = $zlote / $franki;
-$wynike = $zlote / $euro;
-$wynikd = $zlote / $dolary;
-$wynikj = $zlote / $jen;
-$wynikda = $zlote / $dolara;
-$wynikcypr = $zlote / $funtcypr;
-$wynikcz = $zlote / $koronacz;
-$wynikdu = $zlote / $koronadu;
-$wynikdk = $zlote / $dolark;
-$wynikr = $zlote / $rubel;
-$wynikksz = $zlote / $koronasz;
-$wynik = "Obliczenia walut (przybliżone):<BR><BR>$zlote złotych = $wynikf <FONT COLOR=blue>Franków Szwajcarskich.</FONT><BR>$zlote złotych = $wynike <FONT COLOR=blue>Euro.</FONT><BR>$zlote złotych = $wynikd <FONT COLOR=blue>Dolarów Amerykańskich.</FONT><BR>$zlote złotych = $wynikj <FONT COLOR=blue>Jenów Japońskich.</FONT><BR>$zlote złotych = $wynikda <FONT COLOR=blue>Dolarów Australijskich.</FONT><BR>$zlote złotych = $wynikcypr <FONT COLOR=blue>Funtów Cypryjskich.</FONT><BR>$zlote złotych = $wynikcz <FONT COLOR=blue>Koron Czeskich.</FONT><BR>$zlote złotych = $wynikdu <FONT COLOR=blue>Koron Duńskich.</FONT><BR>$zlote złotych = $wynikdk <FONT COLOR=blue>Dolarów Kanadyjskich.</FONT><BR>$zlote złotych = $wynikr <FONT COLOR=blue>Rubli.</FONT><BR>$zlote złotych = $wynikksz <FONT COLOR=blue>Koron Szweckich.</FONT><BR><BR>UWAGA! waluty są przybliżone i mogą różnić się nieco od właściwych kursów walut. ";
+$wynikf = round($zlote / $franki, 2);
+$wynike = round($zlote / $euro,2);
+$wynikd = round($zlote / $dolary,2);
+$wynikj = round($zlote / $jen,2);
+$wynikda = round($zlote / $dolara,2);
+$wynikcypr = round($zlote / $funtcypr,2);
+$wynikcz = round($zlote / $koronacz,2);
+$wynikdu = round($zlote / $koronadu,2);
+$wynikdk = round($zlote / $dolark,2);
+$wynikr = round($zlote / $rubel,2);
+$wynikksz = round($zlote / $koronasz,2);
+$wynik = "Obliczenia walut (przybliżone):<BR><BR>$zlote złotych = $wynikf <FONT COLOR=#56ff00>Franków Szwajcarskich.</FONT><BR>$zlote złotych = $wynike <FONT COLOR=#56ff00>Euro.</FONT><BR>$zlote złotych = $wynikd <FONT COLOR=#56ff00>Dolarów Amerykańskich.</FONT><BR>$zlote złotych = $wynikj <FONT COLOR=#56ff00>Jenów Japońskich.</FONT><BR>$zlote złotych = $wynikda <FONT COLOR=#56ff00>Dolarów Australijskich.</FONT><BR>$zlote złotych = $wynikcypr <FONT COLOR=#56ff00>Funtów Cypryjskich.</FONT><BR>$zlote złotych = $wynikcz <FONT COLOR=#56ff00>Koron Czeskich.</FONT><BR>$zlote złotych = $wynikdu <FONT COLOR=#56ff00>Koron Duńskich.</FONT><BR>$zlote złotych = $wynikdk <FONT COLOR=#56ff00>Dolarów Kanadyjskich.</FONT><BR>$zlote złotych = $wynikr <FONT COLOR=#56ff00>Rubli.</FONT><BR>$zlote złotych = $wynikksz <FONT COLOR=#56ff00>Koron Szweckich.</FONT><BR><BR>UWAGA! waluty są przybliżone i mogą różnić się nieco od właściwych kursów walut. ";
 echo $wynik;
  }
 ?>
 </p>
-</center>
+
     </div>
-    <div class="skryptphp"></div>
+  
 </div>
 
 </body>
